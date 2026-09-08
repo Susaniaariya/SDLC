032fb5e4b1f4b345920dd3d394fb03f87b32cb72
Susani <166315746+Susaniaariya@users.noreply.github.com>
2026-09-08 12:06:41 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/MyNewProject";
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("DB Connection Error:", err));
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hi I am root");
+});
+
+// --- Start server ---
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
